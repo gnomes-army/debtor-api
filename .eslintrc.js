99028563d1b6b5dd0a0bddf5f7716616db01c0c4
Oleg Babenko <majerkravchuk@gmail.com>
2021-01-22 14:49:00 +0300

    Use the airbnb configs for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,37 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+      typescript: {},
+    },
+  },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': ['error', 'never'],
+    'import/no-extraneous-dependencies': 'off',
+    'max-len': ['error', { code: 100, ignoreStrings: true }],
   },
 };

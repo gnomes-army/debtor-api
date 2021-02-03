ba432f8ca01787975952194eb76ed6c6ec768381
Oleg Babenko <majerkravchuk@gmail.com>
2021-02-03 18:51:33 +0300

    Set health-check public

--- src/health-check.controller.ts
+++ src/health-check.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '~modules/auth/decorators/public.decorator';
 
 @Controller()
 export class HealthCheckController {
   @Get()
+  @Public()
   getRoot() {
     return {
       health: 'OK',
